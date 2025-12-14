13dbddfb9513172e4def773aadd4ec5614415c71
Pablit0rg <pablorosagomes@gmail.com>
2025-12-14 12:31:51 -0300

    Delete src directory

--- src/lib/utils.ts
+++
@@ -1,8 +0,0 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-// Essa funçãozinha mágica ajuda a juntar classes do Tailwind de forma condicional e sem conflitos.
-// Vamos usar ela em quase todos os nossos componentes.
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
